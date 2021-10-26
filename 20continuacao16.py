069418e2d2b572fad7b89be2cc5179c6ede600ac
Alana Nascimento de Souza <74723979+alanazip@users.noreply.github.com>
2021-10-26 18:24:02 -0300

    Update 20continuacao16.py

--- 20continuacao16.py
+++ 20continuacao16.py
@@ -14,7 +14,7 @@ for rodada in range(1, tentativas):
         print("Você acertou!")
         break
     elif (maior):
-        print("Você errou. O numero é maior que o numero secreto")
+        print("Você errou. O numero é maior que o número secreto")
     elif (menor):
-        print("Você errou. O numero é menor que o numero secreto")
+        print("Você errou. O numero é menor que o número secreto")
 print("Fim de jogo.")

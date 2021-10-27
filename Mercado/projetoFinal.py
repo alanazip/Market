e748b04d5737b2d2fa5a507b4cc6605977bf029e
Alana Nascimento de Souza <74723979+alanazip@users.noreply.github.com>
2021-10-27 16:40:39 -0300

    Update projetoFinal.py

--- Mercado/projetoFinal.py
+++ Mercado/projetoFinal.py
@@ -8,8 +8,9 @@ preco.append((float(input("Valor R$: \n"))))
 if produto or numero or preco == "exit":
     print("Nome do produto",produto,"Codigo",numero,"Preço R$",preco)
 else:
-    for i in range(1,999):
+    for i in range(1,4):
         print('''<<< Cadastro de produtos - digite exit para sair >>>''')
         produto.append(str (input("Nome do Produto: \n")))
         numero.append((int(input("Número de série: \n"))))
         preco.append((float(input("Valor R$: \n"))))
+print("Nome do produto",produto,"Codigo",numero,"Preço R$",preco)
